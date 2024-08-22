5d20d23801be4cfd983a84352fa3b0d5b1e9255d
ricadolcvuba <132418140+ricadolcvuba@users.noreply.github.com>
2024-08-22 01:20:05 -0300

    funcionando el obtener el hash

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,15 @@ impl From<io::Error> for Errores {
 
 struct Documento{
     nombre : String,
-    id_doc : i32,
+    id_doc: i32,
+}
+impl Documento{
+    pub fn new(nombre:String, id_doc:i32) -> Documento{
+        Documento{
+            nombre,
+            id_doc
+        }
+    }
 }
 
 struct EstadisticaPalabra {
@@ -99,34 +107,51 @@ fn agregar_al_hash(palabra:String, hash : &mut HashMap<String, Vec<EstadisticaPa
     hash
 }
 
-fn leer_todos_los_arch(dir : String) -> Result<HashMap<String, Vec<EstadisticaPalabra>>, Errores>{
+fn leer_todos_los_arch(dir : String, documentos:&mut Vec<Documento>) -> Result<HashMap<String, Vec<EstadisticaPalabra>>, Errores>{
     let mut hash = HashMap::new();
 
-    let entradas_de_arch = fs::read_dir(dir)?;
+    let entradas_de_arch = fs::read_dir(dir);
+
+    let mut id_doc = 0;
 
     //Para cada archivo que sea extencion .txt se va a leer el contenido del documento
-    for entrada_arch in entradas_de_arch {
+    for entrada_arch in entradas_de_arch? {
         let entrada = entrada_arch?; //Devuelve la entrada del inodo leido
         let path_arch = entrada.path(); //Obtiene el path de la entrada del archivo
 
         //Verifica si la entrada leida es un archivo y es una extencion .txt
         if path_arch.is_file() && path_arch.extension().and_then(|s| s.to_str()) == Some("txt"){
+            let nombre_archivo = entrada.file_name().to_str().unwrap().to_string(); //Obtener el nombre del documento
+            documentos.push(Documento::new(nombre_archivo, id_doc as i32));//Colocarlo en el vector de documentos del corpus
             let contenido = leer_arch(&path_arch)?; //Baja todo el contenido del acrhivo
             let contenidos_parseado = parsear_contenido(contenido)?; //Hace el preprocesamiento y la vectorizacion al contenido leido
 
-            for (id_doc, palabra) in contenidos_parseado.iter().enumerate(){
-                if !hash.contains_key(palabra){
-                    let estadistica_palabra = EstadisticaPalabra::new(id_doc as i32, 1);
-                    let mut vector_estadisticas_palabras : Vec<EstadisticaPalabra> = Vec::new();
-                    vector_estadisticas_palabras.push(estadistica_palabra);
-                    hash.insert(palabra.to_string(), vector_estadisticas_palabras);
+            for palabra in contenidos_parseado.iter(){
+                if !hash.contains_key(palabra){//Si no esta contenida la palabra (la clave) en el hashmap entonces colocar la palabra (la clave) como una nueva entrada al hashmap
+                    let estadistica_palabra = EstadisticaPalabra::new(id_doc as i32, 1); //Inicializo las estadisticas de la palabra nueva en el documento presente
+                    let mut vector_estadisticas_palabras : Vec<EstadisticaPalabra> = Vec::new();//Creo un nuevo vector de las estadisticas de la palabras para los documentos que aparezca
+                    vector_estadisticas_palabras.push(estadistica_palabra);//Agrego las estadisticas de la palabra del documento en un vector
+                    hash.insert(palabra.to_string(), vector_estadisticas_palabras);//Agrego la palabra con sus estadisticas en cada documento (hasta ahora es solo un doc) en el hash
                 }
-                else if hash.contains_key(palabra) && hash.get_mut(palabra).unwrap().get_mut(id_doc).unwrap().get_id_doc() == id_doc as i32{
-                    hash.get_mut(palabra).unwrap().get_mut(id_doc).unwrap().sumar_cant();
+                else {
+                    let estadisticas_de_los_docs = hash.get_mut(palabra).unwrap();
+                    let mut encontrado = false;
+                    for estadistica_en_el_doc in estadisticas_de_los_docs.iter_mut() {
+                        if estadistica_en_el_doc.get_id_doc() == id_doc{
+                            estadistica_en_el_doc.sumar_cant();
+                            encontrado = true;
+                            break
+                        }
+                    }
+                    if !encontrado {
+                        let estadistica_palabra = EstadisticaPalabra::new(id_doc, 1);
+                        estadisticas_de_los_docs.push(estadistica_palabra)
+                    }
                 }
             }
 
         }
+        id_doc+=1
     }
 
     Ok(hash)
@@ -146,6 +171,13 @@ pub fn test_parsear_contenido(){
     let resultado = parsear_contenido("El hola como como y\n".to_string()).unwrap();
     let esperado = vec!["hola".to_string(), "como".to_string(), "como".to_string()];
     assert_eq!(resultado, esperado)
+}
+
+#[test]
+pub fn test_leer_todos_los_arch(){
+    let mut vector : Vec<Documento> = Vec::new();
+    let resultado = leer_todos_los_arch("src/corpus".to_string(), &mut vector).unwrap();
+    assert!(resultado.contains_key(&"futbol".to_string()))
 }
 
 fn main() {
